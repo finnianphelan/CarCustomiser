e907f13690550d82dc73dfdc9bae31d709f77b6b
Finnian Phelan <finnianphelan@outlook.com>
2024-01-19 15:21:15 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Finnian Phelan on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
